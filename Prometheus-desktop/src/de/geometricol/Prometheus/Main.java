188d81317d0d81194b2414e97eee26ae0a91e83d
TheRyco <sericose@aol.com>
2014-03-16 19:54:43 +0100

    first commit

---
+++ Prometheus-desktop/src/de/geometricol/Prometheus/Main.java
@@ -0,0 +1,16 @@
+package de.geometricol.Prometheus;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Prometheus";
+		cfg.useGL20 = false;
+		cfg.width = 600;
+		cfg.height = cfg.width /16 *9;
+		cfg.resizable = false;
+		new LwjglApplication(new PrometheusGame(), cfg);
+	}
+}
