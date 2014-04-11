26a0449720d5ddedf5908b6929701b5ea849e23d
TheRyco <sericose@aol.com>
2014-04-11 21:07:52 +0200

    Collision Detection... that doesnt work.

--- Prometheus/src/de/geometricol/Entity/EntityPlayer.java
+++ Prometheus/src/de/geometricol/Entity/EntityPlayer.java
@@ -5,12 +5,13 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 import de.geometricol.Handlers.AssetLoader;
 import de.geometricol.Screens.GameScreen;
 import de.geometricol.WorldNRenderer.GameWorld;
+import de.geometricol.WorldNRenderer.Map;
 import de.geometricol.WorldNRenderer.Tile;
 import de.geometricol.ui.UiHandler;
 
@@ -75,10 +76,10 @@ public class EntityPlayer extends Entity {
 		super.update(delta);
 		playerState = PlayerState.IDLE;
 
-		if (screen.inputHandler.keys[Input.Keys.LEFT] || UiHandler.leftButton.isClicked) playerState = PlayerState.WALKING_LEFT;
-		if (screen.inputHandler.keys[Input.Keys.RIGHT] || UiHandler.rightButton.isClicked) playerState = PlayerState.WALKING_RIGHT;
-		if (screen.inputHandler.keys[Input.Keys.UP] || UiHandler.upButton.isClicked) playerState = PlayerState.WALKING_UP;
-		if (screen.inputHandler.keys[Input.Keys.DOWN] || UiHandler.downButton.isClicked) playerState = PlayerState.WALKING_DOWN;
+		if (screen.inputHandler.keys[Input.Keys.LEFT] || screen.inputHandler.keys[Input.Keys.A] || UiHandler.leftButton.isClicked) playerState = PlayerState.WALKING_LEFT;
+		if (screen.inputHandler.keys[Input.Keys.RIGHT] || screen.inputHandler.keys[Input.Keys.D] || UiHandler.rightButton.isClicked) playerState = PlayerState.WALKING_RIGHT;
+		if (screen.inputHandler.keys[Input.Keys.UP] || screen.inputHandler.keys[Input.Keys.W] || UiHandler.upButton.isClicked) playerState = PlayerState.WALKING_UP;
+		if (screen.inputHandler.keys[Input.Keys.DOWN] || screen.inputHandler.keys[Input.Keys.S] || UiHandler.downButton.isClicked) playerState = PlayerState.WALKING_DOWN;
 
 		switch (playerState) {
 		case IDLE:
@@ -123,29 +124,103 @@ public class EntityPlayer extends Entity {
 	}
 
 	public boolean collidesLeft() {
+		Map currentMap = world.currentMap;
+
+		Vector2 lU, lM, lD;
+		lU = new Vector2(Math.round(position.x) - 1, Math.round(position.y) + 1);
+		lM = new Vector2(Math.round(position.x) - 1, Math.round(position.y));
+		lD = new Vector2(Math.round(position.x) - 1, Math.round(position.y) - 1);
+
+		Tile tLU = currentMap.tiles.get((int) (lU.x + lU.y * currentMap.widthInTiles));
+		Tile tLM = currentMap.tiles.get((int) (lM.x + lM.y * currentMap.widthInTiles));
+		Tile tLD = currentMap.tiles.get((int) (lD.x + lD.y * currentMap.widthInTiles));
+
+		if (tLU.collidable) {
+			if (tLU.bounds.overlaps(bounds)) return true;
+		}
+		if (tLM.collidable) {
+			if (tLM.bounds.overlaps(bounds)) return true;
+		}
+		if (tLD.collidable) {
+			if (tLD.bounds.overlaps(bounds)) return true;
+		}
 
 		
-
 		return false;
 	}
 
 	public boolean collidesRight() {
+		Map currentMap = world.currentMap;
 
-		
+		Vector2 rU, rM, rD;
+		rU = new Vector2(Math.round(position.x) + 1, Math.round(position.y) + 1);
+		rM = new Vector2(Math.round(position.x) + 1, Math.round(position.y));
+		rD = new Vector2(Math.round(position.x) + 1, Math.round(position.y) - 1);
+
+		Tile tRU = currentMap.tiles.get((int) (rU.x + rU.y * currentMap.widthInTiles));
+		Tile tRM = currentMap.tiles.get((int) (rM.x + rM.y * currentMap.widthInTiles));
+		Tile tRD = currentMap.tiles.get((int) (rD.x + rD.y * currentMap.widthInTiles));
+
+		if (tRU.collidable) {
+			if (tRU.bounds.overlaps(bounds)) return true;
+		}
+		if (tRM.collidable) {
+			if (tRM.bounds.overlaps(bounds)) return true;
+		}
+		if (tRD.collidable) {
+			if (tRD.bounds.overlaps(bounds)) return true;
+		}
+
 
 		return false;
 	}
 
 	public boolean collidesUp() {
+		Map currentMap = world.currentMap;
 
-		
+		Vector2 uL, uM, uR;
+		uL = new Vector2(Math.round(position.x) - 1, Math.round(position.y) + 1);
+		uM = new Vector2(Math.round(position.x), Math.round(position.y) + 1);
+		uR = new Vector2(Math.round(position.x) + 1, Math.round(position.y) + 1);
+
+		Tile tUL = currentMap.tiles.get((int) (uL.x + uL.y * currentMap.widthInTiles));
+		Tile tUM = currentMap.tiles.get((int) (uM.x + uM.y * currentMap.widthInTiles));
+		Tile tUR = currentMap.tiles.get((int) (uR.x + uR.y * currentMap.widthInTiles));
+
+		if (tUL.collidable) {
+			if (tUL.bounds.overlaps(bounds)) return true;
+		}
+		if (tUM.collidable) {
+			if (tUM.bounds.overlaps(bounds)) return true;
+		}
+		if (tUR.collidable) {
+			if (tUR.bounds.overlaps(bounds)) return true;
+		}
 
 		return false;
 	}
 
 	public boolean collidesDown() {
+		Map currentMap = world.currentMap;
 
-		
+		Vector2 dL, dM, dR;
+		dL = new Vector2(Math.round(position.x) - 1, Math.round(position.y) - 1);
+		dM = new Vector2(Math.round(position.x), Math.round(position.y) - 1);
+		dR = new Vector2(Math.round(position.x) + 1, Math.round(position.y) - 1);
+
+		Tile tDL = currentMap.tiles.get((int) (dL.x + dL.y * currentMap.widthInTiles));
+		Tile tDM = currentMap.tiles.get((int) (dM.x + dM.y * currentMap.widthInTiles));
+		Tile tDR = currentMap.tiles.get((int) (dR.x + dR.y * currentMap.widthInTiles));
+
+		if (tDL.collidable) {
+			if (tDL.bounds.overlaps(bounds)) return true;
+		}
+		if (tDM.collidable) {
+			if (tDM.bounds.overlaps(bounds)) return true;
+		}
+		if (tDR.collidable) {
+			if (tDR.bounds.overlaps(bounds)) return true;
+		}
 
 		return false;
 	}
--- Prometheus/src/de/geometricol/WorldNRenderer/GameWorld.java
+++ Prometheus/src/de/geometricol/WorldNRenderer/GameWorld.java
@@ -8,22 +8,23 @@ import de.geometricol.Screens.GameScreen;
 public class GameWorld {
 	public GameScreen screen;
 	public Map currentMap, mapDemo;
-	
+
 	public EntityPlayer player;
 
 	public GameWorld(GameScreen screen) {
 		this.screen = screen;
+
 		mapDemo = new Map(Gdx.files.internal("data/MapDemo.png"), false);
+
 		currentMap = mapDemo;
 		player = new EntityPlayer(screen, this);
 		player.debug = true;
-		mapDemo.debug = true;
-		
+
 	}
 
 	public void update(float delta) {
 		player.update(delta);
-		
+
 	}
 
 }
